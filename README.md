df6cc6bd73a33910391fe1bcacc5979b83e0abca
Morsé <hanbunne@gmail.com>
2024-03-25 14:43:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ThaiLocalization
+ Thai Localization mods for Outer Wilds
---
+++ ThaiLocalization/ThaiLocalization.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System;
+
+namespace ThaiLocalization
+{
+    public class ThaiLocalization : ModBehaviour
+    {
+        public static ThaiLocalization Instance;
+        public static string translationFile = "translations/translation.xml";
+
+        private void Awake()
+        {
+        }
+
+        private void Start()
+        {
+            var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+            api.RegisterLanguage(this, "Thai", translationFile);
+			api.AddLanguageFont(this, "Thai", "fonts/kanchamay", "fonts/KanchaMay.ttf");
+
+            //Quark-Bold.otf
+
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"My mod {nameof(ThaiLocalization)} is loaded!", MessageType.Success);
+        }
+    }
+}
